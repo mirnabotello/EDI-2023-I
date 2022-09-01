b8df981d3be000473970013d79221db6a59115da
mirnabotello <mbotello@skytouchtechnology.com>
2022-09-01 09:49:59 -0500

    Agregue ciclo for

--- for.c
+++ for.c
@@ -2,7 +2,21 @@
 
 int main() {
 
-    printf("Hello World!");
+    //  while
+    //  do - while
+    //  for - cuando sabemos cuantas vueltas va a dar
+    //  Sintaxis:  for(inicialización; condicion; incremento/decremento) { // instrucciones }
+
+    int contador;
+    int datoUsuario;
+
+    printf("Dame un número: ");
+    scanf("%d", &datoUsuario);
+
+    for(contador = datoUsuario; contador <= 10; contador++)
+    {
+        printf("%d\n", contador);
+    }
 
     return 0;
 }
